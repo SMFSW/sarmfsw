5a71639876df72d1b7d07c0952c3cb961c18c0bb
Travis CI <travis@travis-ci.org>
2018-12-23 09:26:15 +0000

    Deploy code docs to GitHub Pages Travis build: 28
    Commit: e54fdd102541772292fdd257b1d4453d3bf049b2

--- html/search/all_14.js
+++ html/search/all_14.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['val_5fat',['VAL_AT',['../arm__macros_8h.html#a638d917776f76f7f41be5875d9305f50',1,'arm_macros.h']]],
   ['verbdec',['verbDec',['../arm__stdclib_8h.html#ac3fd4444390849a4408b62064ec70605',1,'arm_stdclib.h']]],
   ['verbinc',['verbInc',['../arm__stdclib_8h.html#a679dacf2212f2c67dcf07cc246a79b0c',1,'arm_stdclib.h']]],
   ['verbinstr',['verbInstr',['../arm__stdclib_8h.html#a7a0958004718cacfebd15dee4a047b58',1,'arm_stdclib.h']]]
