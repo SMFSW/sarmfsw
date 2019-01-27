448fbc8ef330a83836924c2e5fcad95adacaab82
Travis CI <travis@travis-ci.org>
2019-01-27 23:46:59 +0000

    Deploy code docs to GitHub Pages Travis build: 31
    Commit: b88cb57b7da6c01fbeaf7f4344db655deb3e7475

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -8,9 +8,14 @@ var searchData=
   ['charnul',['charNUL',['../arm__macros_8h.html#a7fbb0dad1ca70368008c8f4faff09aef',1,'arm_macros.h']]],
   ['clamp',['CLAMP',['../arm__macros_8h.html#aa1883836bb0955d41ac606649d0cab4b',1,'arm_macros.h']]],
   ['cold_5f_5f',['COLD__',['../arm__attributes_8h.html#a74dad516b3dff433aa6571912ff67df6',1,'arm_attributes.h']]],
+  ['conv16to32bits',['conv16to32Bits',['../arm__inlines_8h.html#a0d019ceba7e589e953ad14552f43baf5',1,'arm_inlines.h']]],
   ['conv16to8bits',['conv16to8Bits',['../arm__inlines_8h.html#a7de2b87520419d797b6a91591014dfb8',1,'arm_inlines.h']]],
   ['conv16upto32bits',['conv16upto32Bits',['../arm__inlines_8h.html#a3d38ca4793f247857de480c56fd386d8',1,'arm_inlines.h']]],
+  ['conv32to16bits',['conv32to16Bits',['../arm__inlines_8h.html#af73e041015a5219dff8dd5249c87850a',1,'arm_inlines.h']]],
+  ['conv32to64bits',['conv32to64Bits',['../arm__inlines_8h.html#ac51e9047ff304b98d9dadf6c218d3c2a',1,'arm_inlines.h']]],
   ['conv32upto64bits',['conv32upto64Bits',['../arm__inlines_8h.html#a3d75eb415c6e1e2934004156649b8abe',1,'arm_inlines.h']]],
+  ['conv64to32bits',['conv64to32Bits',['../arm__inlines_8h.html#aaf54d655508f6b03191b00e861c2257e',1,'arm_inlines.h']]],
   ['conv8to16bits',['conv8to16Bits',['../arm__inlines_8h.html#a7dd927f8650fde0ed0aeced1582657eb',1,'arm_inlines.h']]],
-  ['conv8upto16bits',['conv8upto16Bits',['../arm__inlines_8h.html#a108d66bf0d45f84e85b0f923309ca422',1,'arm_inlines.h']]]
+  ['conv8upto16bits',['conv8upto16Bits',['../arm__inlines_8h.html#a108d66bf0d45f84e85b0f923309ca422',1,'arm_inlines.h']]],
+  ['convxtoybits',['convXtoYBits',['../arm__inlines_8h.html#a4c3a90aca0d0e1849b28e463e294c1e1',1,'arm_inlines.h']]]
 ];
