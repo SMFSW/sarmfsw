434050864a4e51d40d5691ab70301be6a901de4d
Travis CI <travis@travis-ci.org>
2020-10-07 23:30:04 +0000

    Deploy code docs to GitHub Pages Travis build: 49
    Commit: b970590e65bbf88917c466cbf82543b1c00a237d

--- html/search/all_3.js
+++ html/search/all_3.js
@@ -5,7 +5,7 @@ var searchData=
   ['celsius2kelvin',['celsius2kelvin',['../arm__inlines__temperature_8h.html#a7f8e8571f3a41a6cfebffaf5ecbf173e',1,'arm_inlines_temperature.h']]],
   ['char',['CHAR',['../arm__typedefs_8h.html#aebb9e13210d88d43e32e735ada43a425',1,'arm_typedefs.h']]],
   ['charnull',['charNull',['../arm__macros_8h.html#ace955bbe2a2f83133bc0f701e22c08a5',1,'arm_macros.h']]],
-  ['clamp',['CLAMP',['../arm__macros_8h.html#aa1883836bb0955d41ac606649d0cab4b',1,'CLAMP():&#160;arm_macros.h'],['../arm__macros_8h.html#a8594bb7076b41a9e7f7fd061c5e334fd',1,'clamp():&#160;arm_macros.h']]],
+  ['clamp',['CLAMP',['../arm__macros_8h.html#a0912f55b4d17650e8c90ea81c68c69bc',1,'CLAMP():&#160;arm_macros.h'],['../arm__macros_8h.html#a8594bb7076b41a9e7f7fd061c5e334fd',1,'clamp():&#160;arm_macros.h']]],
   ['clr_5fbits',['CLR_BITS',['../arm__macros_8h.html#ae6561729a0a45237a834020a43720afe',1,'arm_macros.h']]],
   ['cold_5f_5f',['COLD__',['../arm__attributes_8h.html#a74dad516b3dff433aa6571912ff67df6',1,'arm_attributes.h']]],
   ['conv16to32bits',['conv16to32Bits',['../arm__inlines__var__size_8h.html#a0d019ceba7e589e953ad14552f43baf5',1,'arm_inlines_var_size.h']]],
