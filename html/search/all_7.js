f4357a29ae12289b528d5475b6e02055421893b9
Travis CI <travis@travis-ci.org>
2021-02-12 11:52:12 +0000

    Deploy code docs to GitHub Pages Travis build: 60
    Commit: 1693bea213b5dfb6b10772068f27a7e52073afd2

--- html/search/all_7.js
+++ html/search/all_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['get_5fbits',['GET_BITS',['../arm__macros_8h.html#a8516892dc13ba56e4f041979646e870c',1,'arm_macros.h']]],
-  ['get_5ffp_5fdec',['get_fp_dec',['../arm__inlines__float_8h.html#ae1608bf9fde5dc01be674f34172151f0',1,'arm_inlines_float.h']]],
+  ['get_5ffp_5fdec',['get_fp_dec',['../arm__inlines__float_8h.html#a7e9d7e76eafaee8d5ba06320b5c5b764',1,'arm_inlines_float.h']]],
   ['getmsbitset',['getMSBitSet',['../arm__inlines__binary_8h.html#a3b4c0ea5c22f86371428b38be1501b3e',1,'arm_inlines_binary.h']]],
   ['gpio',['GPIO',['../arm__chip__stm32_8h.html#ac48e8ecd092bb6be7101cf0195f6e73e',1,'arm_chip_stm32.h']]],
   ['gray2bin',['gray2bin',['../arm__inlines__gray_8h.html#a75b2d190a8d80294c31477c0324e970c',1,'arm_inlines_gray.h']]]
--- html/search/functions_5.js
+++ html/search/functions_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['get_5ffp_5fdec',['get_fp_dec',['../arm__inlines__float_8h.html#ae1608bf9fde5dc01be674f34172151f0',1,'arm_inlines_float.h']]],
+  ['get_5ffp_5fdec',['get_fp_dec',['../arm__inlines__float_8h.html#a7e9d7e76eafaee8d5ba06320b5c5b764',1,'arm_inlines_float.h']]],
   ['getmsbitset',['getMSBitSet',['../arm__inlines__binary_8h.html#a3b4c0ea5c22f86371428b38be1501b3e',1,'arm_inlines_binary.h']]],
   ['gray2bin',['gray2bin',['../arm__inlines__gray_8h.html#a75b2d190a8d80294c31477c0324e970c',1,'arm_inlines_gray.h']]]
 ];
