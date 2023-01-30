b9a4cf36e75d064fc83913463456385ddfdc7429
Travis CI <travis@travis-ci.org>
2023-01-30 10:39:30 +0000

    Deploy code docs to GitHub Pages Travis build: 85
    Commit: a1bfe75e5464f6609e860584d659028d0272af6c

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['_5f_5fasm',['__ASM',['../arm__attributes_8h.html#a1378040bcf22428955c6e3ce9c2053cd',1,'arm_attributes.h']]],
-  ['_5f_5finline',['__INLINE',['../arm__attributes_8h.html#ade2d8d7118f8ff49547f60aa0c3382bb',1,'arm_attributes.h']]],
+  ['_5f_5finline',['__INLINE',['../arm__attributes_8h.html#ade2d8d7118f8ff49547f60aa0c3382bb',1,'__INLINE():&#160;arm_attributes.h'],['../arm__attributes_8h.html#ade2d8d7118f8ff49547f60aa0c3382bb',1,'__INLINE():&#160;arm_attributes.h']]],
   ['_5f_5firq',['__IRQ',['../arm__attributes_8h.html#aa5a6a9b207c15c7edc0f3fbdf74667ce',1,'arm_attributes.h']]],
   ['_5f_5fnoopt_5f_5f',['__NOOPT__',['../arm__attributes_8h.html#ae429abb49d0235f7d07ea426b146cb3b',1,'arm_attributes.h']]],
   ['_5f_5fstatic_5finline',['__STATIC_INLINE',['../arm__attributes_8h.html#aba87361bfad2ae52cfe2f40c1a1dbf9c',1,'arm_attributes.h']]],
