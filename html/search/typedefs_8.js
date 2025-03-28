8092be77ff9c65db36dab5bd9a37f5d091b4a92f
SMFSW <SMFSW@users.noreply.github.com>
2025-03-28 12:00:25 +0100

    Doxygen documentation update

--- html/search/typedefs_8.js
+++ html/search/typedefs_8.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['ubyte_0',['uByte',['../arm__typedefs_8h.html#a15fec02af7f9f531e22853252b3fdcc9',1,'arm_typedefs.h']]],
-  ['udword_1',['uDWord',['../arm__typedefs_8h.html#a99883c8de42951e070918941dda6a008',1,'arm_typedefs.h']]],
-  ['uintcpu_5ft_2',['uintCPU_t',['../arm__typedefs_8h.html#ac78fad3d8bf59eb44161d6fb5f4c73f8',1,'arm_typedefs.h']]],
-  ['ulword_3',['uLWord',['../arm__typedefs_8h.html#aa91c53d2e87fa643d8702f0a01791219',1,'arm_typedefs.h']]],
-  ['uword_4',['uWord',['../arm__typedefs_8h.html#ad416cfc6b2a307c9f0ea40513247a541',1,'arm_typedefs.h']]]
+  ['uchar_1',['UCHAR',['../arm__typedefs_8h.html#a4f4bb67531a9bf6f0b9c6ad76aeba587',1,'arm_typedefs.h']]],
+  ['udword_2',['uDWord',['../arm__typedefs_8h.html#a99883c8de42951e070918941dda6a008',1,'arm_typedefs.h']]],
+  ['uintcpu_5ft_3',['uintCPU_t',['../arm__typedefs_8h.html#ac78fad3d8bf59eb44161d6fb5f4c73f8',1,'arm_typedefs.h']]],
+  ['ulword_4',['uLWord',['../arm__typedefs_8h.html#aa91c53d2e87fa643d8702f0a01791219',1,'arm_typedefs.h']]],
+  ['uword_5',['uWord',['../arm__typedefs_8h.html#ad416cfc6b2a307c9f0ea40513247a541',1,'arm_typedefs.h']]]
 ];
