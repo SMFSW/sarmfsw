e789f11046fc325660a246d313542d2958e9f89a
SMFSW <SMFSW@users.noreply.github.com>
2025-03-29 11:21:20 +0100

    Doxygen documentation update

--- html/search/all_13.js
+++ html/search/all_13.js
@@ -2,16 +2,15 @@ var searchData=
 [
   ['ubyte_0',['uByte',['../unionu_byte.html',1,'uByte'],['../arm__typedefs_8h.html#a15fec02af7f9f531e22853252b3fdcc9',1,'uByte:&#160;arm_typedefs.h']]],
   ['ubyte_5fi_1',['uByte_i',['../unionu_byte__i.html',1,'']]],
-  ['uchar_2',['UCHAR',['../arm__typedefs_8h.html#a4f4bb67531a9bf6f0b9c6ad76aeba587',1,'arm_typedefs.h']]],
-  ['udword_3',['uDWord',['../unionu_d_word.html',1,'uDWord'],['../arm__typedefs_8h.html#a99883c8de42951e070918941dda6a008',1,'uDWord:&#160;arm_typedefs.h']]],
-  ['udword_5fi_4',['uDWord_i',['../unionu_d_word__i.html',1,'']]],
-  ['uintcpu_5ft_5',['uintCPU_t',['../arm__typedefs_8h.html#ac78fad3d8bf59eb44161d6fb5f4c73f8',1,'arm_typedefs.h']]],
-  ['ulword_6',['uLWord',['../unionu_l_word.html',1,'uLWord'],['../arm__typedefs_8h.html#aa91c53d2e87fa643d8702f0a01791219',1,'uLWord:&#160;arm_typedefs.h']]],
-  ['ulword_5fi_7',['uLWord_i',['../unionu_l_word__i.html',1,'']]],
-  ['undefined_8',['Undefined',['../arm__macros_8h.html#a9a257c75902e62f9645e4053418b0edf',1,'arm_macros.h']]],
-  ['unused_9',['UNUSED',['../arm__cmsis_8h.html#a6bc306171da085f4c7df2c66d2ff8e47',1,'arm_cmsis.h']]],
-  ['unused_5fret_10',['UNUSED_RET',['../arm__cmsis_8h.html#a043d1cd6633a6bd80d29a3bf3e5d842f',1,'arm_cmsis.h']]],
-  ['used_5f_5f_11',['USED__',['../arm__attributes_8h.html#ab28b7e20751ce7fd19d09e80356ebd8a',1,'arm_attributes.h']]],
-  ['uword_12',['uWord',['../unionu_word.html',1,'uWord'],['../arm__typedefs_8h.html#ad416cfc6b2a307c9f0ea40513247a541',1,'uWord:&#160;arm_typedefs.h']]],
-  ['uword_5fi_13',['uWord_i',['../unionu_word__i.html',1,'']]]
+  ['udword_2',['uDWord',['../unionu_d_word.html',1,'uDWord'],['../arm__typedefs_8h.html#a99883c8de42951e070918941dda6a008',1,'uDWord:&#160;arm_typedefs.h']]],
+  ['udword_5fi_3',['uDWord_i',['../unionu_d_word__i.html',1,'']]],
+  ['uintcpu_5ft_4',['uintCPU_t',['../arm__typedefs_8h.html#ac78fad3d8bf59eb44161d6fb5f4c73f8',1,'arm_typedefs.h']]],
+  ['ulword_5',['uLWord',['../unionu_l_word.html',1,'uLWord'],['../arm__typedefs_8h.html#aa91c53d2e87fa643d8702f0a01791219',1,'uLWord:&#160;arm_typedefs.h']]],
+  ['ulword_5fi_6',['uLWord_i',['../unionu_l_word__i.html',1,'']]],
+  ['undefined_7',['Undefined',['../arm__macros_8h.html#a9a257c75902e62f9645e4053418b0edf',1,'arm_macros.h']]],
+  ['unused_8',['UNUSED',['../arm__cmsis_8h.html#a6bc306171da085f4c7df2c66d2ff8e47',1,'arm_cmsis.h']]],
+  ['unused_5fret_9',['UNUSED_RET',['../arm__cmsis_8h.html#a043d1cd6633a6bd80d29a3bf3e5d842f',1,'arm_cmsis.h']]],
+  ['used_5f_5f_10',['USED__',['../arm__attributes_8h.html#ab28b7e20751ce7fd19d09e80356ebd8a',1,'arm_attributes.h']]],
+  ['uword_11',['uWord',['../unionu_word.html',1,'uWord'],['../arm__typedefs_8h.html#ad416cfc6b2a307c9f0ea40513247a541',1,'uWord:&#160;arm_typedefs.h']]],
+  ['uword_5fi_12',['uWord_i',['../unionu_word__i.html',1,'']]]
 ];
