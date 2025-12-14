a825ec883feabc56e51f2f5d3032cc94eaffae0b
SMFSW <xgarmanboziax@gmail.com>
2025-12-14 23:24:47 +0100

    internal cppcheck runner

---
+++ .cppcheck/cppcheck.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# before first commit, set the script as executable to git:
+# git update-index --chmod=+x .cppcheck/cppcheck.sh
+
+exec cppcheck \
+  --enable=warning,style,performance \
+  --inconclusive \
+  --inline-suppr \
+  --force \
+  --check-level=exhaustive \
+  --suppress=missingIncludeSystem \
+  --suppressions-list=./.cppcheck/cppcheck.suppress \
+  --error-exitcode=1 \
+  --platform=mips32 \
+  '-D__cppcheck__' \
+  '-D__GNUC__' \
+  '-DNONNULLX__(...)=""' \
+  '-DSECTION__(...)=""' \
+  '-D__SIZEOF_INT__=4' \
+  '-D__SIZEOF_POINTER__=4' \
+  .
---
+++ sarmfsw.c
@@ -0,0 +1,8 @@
+/*!\file sarmfsw.c
+** \author SMFSW
+** \copyright MIT (c) 2017-2025, SMFSW
+** \brief sarmfsw include (for cppcheck purpose)
+*/
+/****************************************************************/
+
+#include "sarmfsw.h"
